69bf881cf254fc54e4177e2b75cd7d1e4845ff47
sumit <rajaksumit218@gmail.com>
2023-02-14 13:37:42 +0530

    first commit

--- src/main/java/com/margosa/crmapp/CrmappApplication.java
+++ src/main/java/com/margosa/crmapp/CrmappApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class CrmappApplication {
 
 	public static void main(String[] args) {
